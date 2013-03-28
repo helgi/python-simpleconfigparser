59e8b457b3991ae60dc7e1fa6773d9a60980c196
helgi <helgith@gmail.com>
2013-03-28 22:01:17 +0000

    Add more information

--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from os.path import join, dirname
+
+
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
 
 PKG = 'SimpleConfigParser'
 VERSION = '0.1.0'
 
-setup(name=PKG,
-      version=VERSION,
-      description="Simplified ConfigParser",
-      author="Helgi Þorbjörnsson",
-      author_email="helgi@php.net",
-      url="http://github.com/helgi/python-simpleconfigparser",
-      packages=find_packages(),
-      install_requires=[],
-      license="MIT License",
-      keywords="ConfiParser,ini",
-      zip_safe=True,
-      test_suite="tests",
-      tests_require=[]
+setup(
+    name=PKG,
+    version=VERSION,
+    description="Simplified ConfigParser",
+    long_description=read('README'),
+    author="Helgi Þorbjörnsson",
+    author_email="helgi@php.net",
+    url="http://github.com/helgi/python-simpleconfigparser",
+    packages=find_packages(),
+    install_requires=[],
+    license="MIT License",
+    keywords="ConfiParser,ini",
+    zip_safe=True,
+    test_suite="tests",
+    tests_require=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ]
 )
